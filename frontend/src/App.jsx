6a3db1fb266a4c0711b73f56fff28ac12bd3fd1b
Crixls <crixismundial@gmail.com>
2024-06-03 19:51:33 +0200

    commit 32/06/2024

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,12 @@ import ShoePage from "./pages/ShoePage/ShoePage";
 import FutbolPage from "./pages/FutbolPage/FutbolPage";
 import BasketPage from "./pages/BasketPage/BasketPage";
 import BadmintonPage from "./pages/BadmintonPage/BadmintonPage";
-import RunningPage from "./pages/RunningPage/RunningPage";
-import FitnessPage from "./pages/FitnessPage/FitnessPage";
+import RunningPage from "./pages/RunningPage.jsx/RunningPage";
+import FitnessPage from "./pages/FitnessPage.jsx/FitnessPage";
 import TenisPage from "./pages/TenisPage/TenisPage";
 import SearchPage from "./pages/SearchPage/SearchPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import ProtectedRoute from "./pages/utils/ProtectedRoute";
 import AdminPage from "./pages/AdminPage/AdminPage";
 import AdminPageProducts from "./pages/AdminPage/AdminPageProducts";
 import AdminPageUsers from "./pages/AdminPage/AdminPageUsers";
@@ -33,7 +32,7 @@ import ErrorCommercePage from "./pages/ErrorCommercePage";
 
 function App() {
   const [userLogged, setUserLogged] = useState(null);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isadmin, setisadmin] = useState(false);
 
   useEffect(() => {
     const storedUser = localStorage.getItem('UserToken');
@@ -45,48 +44,50 @@ function App() {
   useEffect(() => {
     const userId = localStorage.getItem('userId');
     console.log(userId);
-    setIsAdmin(Number(userId) === 3);
+    setisadmin(Number(userId) === 3);
   }, []);
-  
+
   return (
     <HashRouter>
       <AuthProvider>
         <EntitiesProvider>
-          <Header />
-          <Routes>
-            {/* Rutas públicas sin Header y Footer */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+          <ErrorBoundary>
+            <Header />
+            <Routes>
+              {/* Rutas públicas sin Header y Footer */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
 
-            {/* Rutas principales con Header y Footer */}
-            <Route path="/" element={<Home />} />
-            <Route path="/man" element={<ManPage />} />
-            <Route path="/woman" element={<WomanPage />} />
-            <Route path="/kids" element={<KidsPage />} />
-            <Route path="/shoe" element={<ShoePage />} />
-            <Route path="/futbol" element={<FutbolPage />} />
-            <Route path="/basket" element={<BasketPage />} />
-            <Route path="/badminton" element={<BadmintonPage />} />
-            <Route path="/running" element={<RunningPage />} />
-            <Route path="/fitness" element={<FitnessPage />} />
-            <Route path="/tenis" element={<TenisPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="*" element={<ErrorCommercePage />} />
+              {/* Rutas principales con Header y Footer */}
+              <Route path="/" element={<Home />} />
+              <Route path="man" element={<ManPage />} />
+              <Route path="woman" element={<WomanPage />} />
+              <Route path="kids" element={<KidsPage />} />
+              <Route path="shoe" element={<ShoePage />} />
+              <Route path="futbol" element={<FutbolPage />} />
+              <Route path="basket" element={<BasketPage />} />
+              <Route path="badminton" element={<BadmintonPage />} />
+              <Route path="running" element={<RunningPage />} />
+              <Route path="fitness" element={<FitnessPage />} />
+              <Route path="tenis" element={<TenisPage />} />
+              <Route path="search" element={<SearchPage />} />
+              <Route path="*" element={<ErrorCommercePage />} />
 
-            {/* Rutas de administración */}
-            <Route path="/admin" element={isAdmin ? <AdminPage /> : <Navigate to="/" />} />
-            <Route path="/admin/products" element={isAdmin ? <AdminPageProducts /> : <Navigate to="/" />} />
-            <Route path="/admin/users" element={isAdmin ? <AdminPageUsers /> : <Navigate to="/" />} />
-            <Route path="/admin/estadisticas" element={isAdmin ? <AdminEstadisticas /> : <Navigate to="/" />} />
+              {/* Rutas de administración */}
+              <Route path="admin" element={isadmin ? <AdminPage /> : <Navigate to="/" />} />
+              <Route path="admin/products" element={isadmin ? <AdminPageProducts /> : <Navigate to="/" />} />
+              <Route path="admin/users" element={isadmin ? <AdminPageUsers /> : <Navigate to="/" />} />
+              <Route path="admin/estadisticas" element={isadmin ? <AdminEstadisticas /> : <Navigate to="/" />} />
 
-            {/* Rutas protegidas para usuarios autenticados */}
-            <Route path="/favorites" element={userLogged ? <FavoritePage /> : <Navigate to="/login" />} />
-            <Route path="/payment" element={userLogged ? <PayPage /> : <Navigate to="/login" />} />
-            <Route path="/profile" element={userLogged ? <ProfilePage /> : <Navigate to="/login" />} />
-            <Route path="/orders" element={userLogged ? <OrdersPage /> : <Navigate to="/login" />} />
-            <Route path="/cart" element={userLogged ? <CartPage /> : <Navigate to="/login" />} />
-          </Routes>
-          <Footer />
+              {/* Rutas protegidas para usuarios autenticados */}
+              <Route path="favorites" element={userLogged ? <FavoritePage /> : <Navigate to="/login" />} />
+              <Route path="payment" element={userLogged ? <PayPage /> : <Navigate to="/login" />} />
+              <Route path="profile" element={userLogged ? <ProfilePage /> : <Navigate to="/login" />} />
+              <Route path="orders" element={userLogged ? <OrdersPage /> : <Navigate to="/login" />} />
+              <Route path="cart" element={userLogged ? <CartPage /> : <Navigate to="/login" />} />
+            </Routes>
+            <Footer />
+          </ErrorBoundary>
         </EntitiesProvider>
       </AuthProvider>
     </HashRouter>
